bf401efe11a73c82e6ff26b732c66eda8a889a09
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-06-25 00:26:16 +0530

    Home index page implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import HomeIndex from "../pages/HomeIndex";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomeIndex />} />
       </Routes>
     </BrowserRouter>
   );
