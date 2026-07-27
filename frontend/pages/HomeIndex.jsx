6b3ba9ccfa61ab4d5fec3d2612b309fcb088589a
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-07-27 12:43:13 +0530

    Index home webpage name change

--- frontend/pages/HomeIndex.jsx
+++ frontend/pages/HomeIndex.jsx
@@ -14,7 +14,7 @@ function Home() {
 
   useEffect(() => {
     const fetchHomes = async () => {
-      document.title = "Index Homes";
+      document.title = "Homes | SmartStay";
       try {
         const response = await axios.get(
           "https://smartstay-d8sz.onrender.com/",
